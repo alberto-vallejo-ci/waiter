b9f2aac4121efb3db0449c8cf3721d64eea6bbee
Alberto Vallejo <javr1088@gmail.com>
2017-05-13 19:04:24 -0500

    Refactor componenst

---
+++ app/assets/javascripts/components/dashboard.jsx
@@ -0,0 +1,11 @@
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className='row'>
+        <div className='col-md-8'>
+          <div className='menu'></div>
+        </div>
+      </div>
+    )
+  }
+}
